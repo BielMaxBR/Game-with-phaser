75fb403154d19e7e6d48e758c970878a3c3f7749
Gabriel Maximo <gbielmaximo@gmail.com>
2021-07-03 19:04:54 +0000

    só isso?

--- assets/Scripts/Scene1.js
+++ assets/Scripts/Scene1.js
@@ -4,31 +4,32 @@ export default class Scene1 extends Phaser.Scene {
   construct() {
     //super({key: "Scene1"})
   }
-  
+
   preload() {
     this.load.spritesheet("player", "assets/Images/spritesheet player.png", {
       frameWidth: 200,
       frameHeight: 320
     })
   }
-  
+
   create() {
     this.player = this.add.sprite(150, 150, "player")
-    
+
     this.player.setScale(0.2, 0.2)
-    
-    this.keyClass = new Keys()
-    
-    console.log()
+
+    this.keyClass = {
+      a: this.input.keyboard.addKey("A")
+    }
+
   }
-  
-  update(delta) {
+
+  update() {
     this.walk()
   }
-  
+
   walk() {
-    if(this.keyClass.a.isDown) {
-      console.log("Letra a apertada")
+    if (this.keyClass.a.isDown) {
+      this.player.x -= 1
     }
   }
 }
--- assets/Scripts/defKeys.js
+++ assets/Scripts/defKeys.js
@@ -1,17 +1,6 @@
-import Scene1 from "./Scene1.js"
-
 export default class keys extends Phaser.Input.Keyboard.KeyboardPlugin {
-  constructor() {
-    super(Scene1)
-  }
-  
-  create() {
-    this.keys = new KeyboardPlugin(Scene1)
-    
+  constructor(scene) {
+    super(scene)
     this.a = this.keys.addKey("A")
-  }
-  
-  update() {
-    
   }
 }
